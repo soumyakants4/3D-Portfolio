e38dcef4d5e40c635bf45b9aed0c6ea6edc9fdac
Soumyakant Sahoo <soumyakants4@gmail.com>
2023-03-28 21:11:06 +0530

    Website-Over

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,10 @@ import styled from "styled-components";
 const Section = styled.div`
   display: flex;
   justify-content: center;
+
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+  } ;
 `;
 const Container = styled.div`
   width: 1400px;
@@ -11,6 +15,11 @@ const Container = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 10px 0px;
+
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    padding: 10px;
+  } ;
 `;
 const Links = styled.div`
   display: flex;
@@ -24,6 +33,9 @@ const List = styled.ul`
   display: flex;
   gap: 30px;
   list-style: none;
+  @media only screen and (max-width: 768px) {
+    display: none;
+  } ;
 `;
 const ListItem = styled.li``;
 
